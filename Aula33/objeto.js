cac56aef71f44e18c9fb7cde7cec2dfb16af0684
edinelson <edinelsonlima86@gmail.com>
2021-04-22 18:38:50 -0300

    Objeto

---
+++ Aula33/objeto.js
@@ -0,0 +1,13 @@
+let amigo = {
+    nome: 'Edinelson',
+    sexo: 'M',
+    idade: '22',
+    peso: 60,
+    engordar(p=0){
+        console.log(`Engordou ${p}kg`)
+        this.peso += p
+    }
+}
+
+amigo.engordar(5)
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
